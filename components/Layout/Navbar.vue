d3dde167b6e3bfa4a99c3afe40d3d2756e0efc4a
Francisco Rivas <jlfr2077@gmail.com>
2023-08-27 20:00:01 -0400

    card section

--- components/Layout/Navbar.vue
+++ components/Layout/Navbar.vue
@@ -11,24 +11,5 @@ const lg = breakpoints.greaterOrEqual("md");
     <div class="flex">
       <BaseText>Navbar </BaseText>
     </div>
-    <div class="flex md:justify-between md:col-end-4">
-      <BaseButtonSecondary icon="/svg/angle-down.svg"
-        >First</BaseButtonSecondary
-      >
-      <BaseButtonSecondary icon="/svg/angle-down.svg"
-        >Second</BaseButtonSecondary
-      >
-      <BaseButtonSecondary v-if="lg">Third</BaseButtonSecondary>
-      <BaseButtonSecondary v-if="lg">Fourth</BaseButtonSecondary>
-      <BaseButtonSecondary v-if="lg">Fifth</BaseButtonSecondary>
-      <nuxt-img
-        v-if="md"
-        src="/svg/menu.svg"
-        height="15"
-        width="15"
-        alt="menu"
-        class="mx-2"
-      />
-    </div>
   </nav>
 </template>
---
+++ components/base/Card.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+interface Props {
+  title: string;
+  posts: { img: string; title: string; link: string; date: string }[];
+}
+const props = defineProps<Props>();
+</script>
+<template>
+  <div
+    class="mx-4 sm:mx-12 lg:mx-48 px-4 lg:px-8 py-12 mt-12 rounded-lg bg-slate-800"
+  >
+    <header>
+      <h2 class="uppercase text-gray-300 text-2xl md:text-3xl font-bold mb-8">
+        {{ props.title }}
+      </h2>
+    </header>
+    <section
+      class="grid grid-cols-2 md:grid-cols-4 grid-rows-4 md:grid-rows-2 gap-y-6 gap-x-8"
+    >
+      <div
+        class="rounded-lg border border-gray-600 flex flex-wrap p-3 hover:bg-slate-900 hover:opacity-95 duration-100"
+        v-for="post in props.posts"
+      >
+        <NuxtLink :to="post.link">
+          <nuxt-img
+            v-bind:src="post.img"
+            class="md:w-[328px] md:h-[260px] flex-grow"
+          />
+        </NuxtLink>
+        <NuxtLink :to="post.link">
+          <h4 class="text-xs sm:text-base font-semibold py-2">
+            {{ post.title }}
+          </h4>
+        </NuxtLink>
+      </div>
+    </section>
+  </div>
+</template>
