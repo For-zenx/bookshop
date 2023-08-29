a409f784ad204f149fcad73885fc5ab884909949
Francisco Rivas <jlfr2077@gmail.com>
2023-08-29 18:02:40 -0400

    Api consume

--- pages/index.vue
+++ pages/index.vue
@@ -17,58 +17,6 @@ definePageMeta({
 <template>
   <main class="body">
     <BaseTitle title="bookshop online" />
-    <BaseCard
-      title="Books Section"
-      :posts="[
-        {
-          title: 'The Lord of the Rings The Fellowship of the Ring',
-          date: '8 november, 2020',
-          img: '/img/lotr.png',
-          link: '',
-        },
-        {
-          title: 'The Lord of the Rings The Fellowship of the Ring',
-          date: '8 november, 2020',
-          img: '/img/lotr2.png',
-          link: '',
-        },
-        {
-          title: 'The Lord of the Rings The Fellowship of the Ring',
-          date: '8 november, 2020',
-          img: '/img/lotr3.png',
-          link: '',
-        },
-        {
-          title: 'The Lord of the Rings The Fellowship of the Ring',
-          date: '8 november, 2020',
-          img: '/img/ten-little-niggers.png',
-          link: '',
-        },
-        {
-          title: 'The Lord of the Rings The Fellowship of the Ring',
-          date: '8 november, 2020',
-          img: '/img/got.png',
-          link: '',
-        },
-        {
-          title: 'The Lord of the Rings The Fellowship of the Ring',
-          date: '8 november, 2020',
-          img: '/img/got2.png',
-          link: '',
-        },
-        {
-          title: 'The Lord of the Rings The Fellowship of the Ring',
-          date: '8 november, 2020',
-          img: '/img/clean-code.png',
-          link: '',
-        },
-        {
-          title: 'The Lord of the Rings The Fellowship of the Ring',
-          date: '8 november, 2020',
-          img: '/img/eloquentjs.png',
-          link: '',
-        },
-      ]"
-    />
+    <BaseBooksBox title="Books Section" />
   </main>
 </template>
